6702d88c6ddc4194bb1a934bb10855c912001d89
Annika Dietz <annika@dietzwolfgang.de>
2019-03-31 20:33:52 +0200

    Adding Quick sort for Stack

--- src/com/os/QuickSorter.java
+++ src/com/os/QuickSorter.java
@@ -3,6 +3,11 @@ package com.os;
 
 public class QuickSorter {
 
+    /**
+     * Sorts a Linked list using quick sort and returns it sorted
+     * @param list List to be sorted
+     * @return sorted list
+     */
     public MyLinkedList<Pokemon> quickSort(MyLinkedList<Pokemon> list) {
         int left = 0;
         int right = list.size() - 1;
@@ -10,6 +15,13 @@ public class QuickSorter {
         return quickSort(list, left, right);
     }
 
+    /**
+     * recursive call to sort a list with a given beginning point and ending point
+     * @param list list to be sorted
+     * @param left beginning
+     * @param right end
+     * @return sorted list
+     */
     private MyLinkedList<Pokemon> quickSort(MyLinkedList<Pokemon> list, int left, int right) {
         if(left < right) {
             int partitionIndex = partition(list, left, right);
@@ -20,7 +32,13 @@ public class QuickSorter {
         return list;
     }
 
-
+    /**
+     * calculates the new pivot place and swaps the parts to the right positions
+     * @param list list to be sorted
+     * @param left beginning index of the list
+     * @param right ending index of the list
+     * @return position of the pivot with which everything was compared
+     */
     private int partition(MyLinkedList<Pokemon> list, int left, int right) {
         Pokemon pivot = list.get(right).getData();
         int i = left - 1;
@@ -37,6 +55,114 @@ public class QuickSorter {
         list.swap(list.get(right), list.get(i+1));
 
         return i+1;
+    }
+
+    /**
+     * Sorts a queue using quick sort
+     * @param queue queue to be sorted
+     * @return sorted queue
+     */
+    public MyQueue<Pokemon> quickSort(MyQueue<Pokemon> queue) {
+        Pokemon pivot = queue.peek();
+
+        //Three new queues, one for smaller elements, one for bigger and one for the pivot element
+        MyQueue<Pokemon> left = new MyQueue<>();
+        MyQueue<Pokemon> right = new MyQueue<>();
+        MyQueue<Pokemon> equals = new MyQueue<>();
+
+        //adding next element to the corresponding queue
+        while (!queue.isEmpty()) {
+            Pokemon element = queue.delete();
+            if (element.compareTo(pivot) < 0) {
+                left.insert(element);
+            } else if (element.compareTo(pivot) == 0) {
+                equals.insert(element);
+            } else {
+                right.insert(element);
+            }
+        }
+
+        //recursive part, as long as there is more than 1 element in each list
+        if(left.size() > 1) {
+            quickSort(left);
+        }
+        if(right.size() > 1) {
+            quickSort(right);
+        }
+
+        //merging the queues in the right order
+        while(!left.isEmpty()) {
+            queue.insert(left.delete());
+        }
+        while(!equals.isEmpty()) {
+            queue.insert(equals.delete());
+        }
+        while(!right.isEmpty()) {
+            queue.insert(right.delete());
+        }
+
+        return queue;
+    }
+
+    /**
+     * Sorts a stack using quick sort
+     * @param stack stack to be sorted
+     * @return sorted stack
+     */
+    public Stack<Pokemon> quickSort(Stack<Pokemon> stack) {
+        Pokemon pivot = stack.peek();
+
+        //Three new queues, one for smaller elements, one for bigger and one for the pivot element
+        Stack<Pokemon> down = new Stack<>();
+        Stack<Pokemon> top = new Stack<>();
+        Stack<Pokemon> equals = new Stack<>();
+
+        //adding next element to the corresponding queue
+        while (!stack.isEmpty()) {
+            Pokemon element = stack.pop();
+            if (element.compareTo(pivot) < 0) {
+                down.push(element);
+            } else if (element.compareTo(pivot) == 0) {
+                equals.push(element);
+            } else {
+                top.push(element);
+            }
+        }
+
+        //recursive part, as long as there is more than 1 element in each list
+        if(down.size() > 1) {
+            quickSort(down);
+        }
+        if(top.size() > 1) {
+            quickSort(top);
+        }
+
+
+        //merging the stacks in the right order, first adding everything to a temporary stack and then to the original
+        //to keep it in the same order
+        Stack<Pokemon> temp = new Stack<>();
+        while(!down.isEmpty()) {
+            temp.push(down.pop());
+        }
+        while(!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+
+        while(!equals.isEmpty()) {
+            temp.push(equals.pop());
+        }
+        while(!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+
+        while(!top.isEmpty()) {
+            temp.push(top.pop());
+        }
+        while(!temp.isEmpty()) {
+            stack.push(temp.pop());
+        }
+
+        return stack;
     }
 
 }
--- test/com/os/QuickSorterTest.java
+++ test/com/os/QuickSorterTest.java
@@ -32,13 +32,77 @@ class QuickSorterTest {
 
         MyLinkedList<Pokemon> sorted = sorter.quickSort(list);
 
-
-
         assertEquals(compareList.get(0).getData(), sorted.get(0).getData()); //five
         assertEquals(compareList.get(1).getData(), sorted.get(1).getData()); //four
         assertEquals(compareList.get(2).getData(), sorted.get(2).getData()); //one
         assertEquals(compareList.get(3).getData(), sorted.get(3).getData()); //three
         assertEquals(compareList.get(4).getData(), sorted.get(4).getData()); //two
+
+    }
+
+    @Test
+    void testQuickSortQueue() {
+        MyQueue<Pokemon> queue = new MyQueue<>();
+        MyQueue<Pokemon> compareQuee = new MyQueue<>();
+        QuickSorter sorter = new QuickSorter();
+        Pokemon pokemon = new Pokemon(1, "One", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon1 = new Pokemon(2, "Two", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon2 = new Pokemon(3, "Three", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon3 = new Pokemon(4, "Four", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon4 = new Pokemon(5, "Five", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+
+        queue.insert(pokemon);
+        queue.insert(pokemon1);
+        queue.insert(pokemon2);
+        queue.insert(pokemon3);
+        queue.insert(pokemon4);
+
+        compareQuee.insert(pokemon4);
+        compareQuee.insert(pokemon3);
+        compareQuee.insert(pokemon);
+        compareQuee.insert(pokemon2);
+        compareQuee.insert(pokemon1);
+
+        MyQueue<Pokemon> sorted = sorter.quickSort(queue);
+
+        assertEquals(compareQuee.delete(), queue.delete()); //five
+        assertEquals(compareQuee.delete(), queue.delete()); //four
+        assertEquals(compareQuee.delete(), queue.delete()); //one
+        assertEquals(compareQuee.delete(), queue.delete()); //three
+        assertEquals(compareQuee.delete(), queue.delete()); //two
+
+    }
+
+    @Test
+    void testQuickSortStack() {
+        Stack<Pokemon> stack = new Stack<>();
+        Stack<Pokemon> compareStack = new Stack<>();
+        QuickSorter sorter = new QuickSorter();
+        Pokemon pokemon = new Pokemon(1, "One", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon1 = new Pokemon(2, "Two", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon2 = new Pokemon(3, "Three", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon3 = new Pokemon(4, "Four", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+        Pokemon pokemon4 = new Pokemon(5, "Five", "Fire", "Water", 500, 4, 4, 4, 4, 4, 4, 4, false);
+
+        stack.push(pokemon);
+        stack.push(pokemon1);
+        stack.push(pokemon2);
+        stack.push(pokemon3);
+        stack.push(pokemon4);
+
+        compareStack.push(pokemon4);
+        compareStack.push(pokemon3);
+        compareStack.push(pokemon);
+        compareStack.push(pokemon2);
+        compareStack.push(pokemon1);
+
+        Stack<Pokemon> sorted = sorter.quickSort(stack);
+
+        assertEquals(compareStack.pop(), stack.pop()); //five
+        assertEquals(compareStack.pop(), stack.pop()); //four
+        assertEquals(compareStack.pop(), stack.pop()); //one
+        assertEquals(compareStack.pop(), stack.pop()); //three
+        assertEquals(compareStack.pop(), stack.pop()); //two
 
     }
 

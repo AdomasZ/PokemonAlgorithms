a408682874ce107af4b2c980d40c54b603977c22
AdomasZ <AdomasZ@users.noreply.github.com>
2019-04-01 13:02:17 +0200

    Updated DoublyLinkedList and sorter

--- src/com/os/Sorter.java
+++ src/com/os/Sorter.java
@@ -1,7 +1,44 @@
 package com.os;
 
 public class Sorter {
-
+    public MyLinkedList<Pokemon> bubbleSort(MyLinkedList<Pokemon> myLinkedList){
+        int listSize = myLinkedList.size();
+        boolean didSwap;
+        for (int i = 0; i < listSize-1; i++) {
+            didSwap = false;
+            // The highest element always ends up at the end of the list after a pass. After every pass and element put in its place at the end to the list.
+            // That's why evey time we check one element less.
+            for (int j = 0; j < listSize-i-1; j++){
+                if (myLinkedList.get(j).getData().compareTo(myLinkedList.get(j + 1).getData()) > 0) {
+                    myLinkedList.swap(myLinkedList.get(j), myLinkedList.get(j+1));
+                    didSwap= true;
+                }
+            }
+            if(!didSwap){
+                break;
+            }
+        }
+        return myLinkedList;
+    }
+    public MyDoublyLinkedList<Pokemon> bubbleSort(MyDoublyLinkedList<Pokemon> myLinkedList){
+        int listSize = myLinkedList.size();
+        boolean didSwap;
+        for (int i = 0; i < listSize-1; i++) {
+            didSwap = false;
+            // The highest element always ends up at the end of the list after a pass. After every pass and element put in its place at the end to the list.
+            // That's why evey time we check one element less.
+            for (int j = 0; j < listSize-i-1; j++){
+                if (myLinkedList.get(j).getData().compareTo(myLinkedList.get(j + 1).getData()) > 0) {
+                    myLinkedList.swapRight(myLinkedList.get(j));
+                    didSwap = true;
+                }
+            }
+            if(!didSwap){
+                break;
+            }
+        }
+        return myLinkedList;
+    }
 
 
 }
